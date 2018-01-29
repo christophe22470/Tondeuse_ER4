7ea5cbf7b310487b947719814e34e82fcbced30b
krikristoophe <christophe22470@gmail.com>
2018-01-29 23:23:15 +0100

    Remise en ordre

--- tondeuse/tondeuse.ino
+++ tondeuse/tondeuse.ino
@@ -1,38 +1,58 @@
 #include <SoftwareSerial.h>
-// lien avec le module bluetooth
+#include <MsTimer2.h>
 
+//Lien avec le module bluetooth
 #define RxD 2
 #define TxD 3
 
-//Arduino PWM Speed Control：
-int E1 = 5;   
-int M1 = 4; 
-int E2 = 6;                       
-int M2 = 7;
-int CGAUCHE=9;
-int CDROITE=8;
-int vitesse=255;
- 
-#define GAUCHE 1
-#define DROITE 2
-#define DEVANT 3 
-#define ARRIERE 4
+//Paramètres moteur coupe
+#define Km 0.3
+#define Kmv 0.4
+#define PDDMoteur 0.5
+#define pinTensionMoteur A0
+#define pinMoteurCoupe 8
+double consigneVitesse = 0.0;
 
-SoftwareSerial BLE(RxD,TxD);
+//Paramètres PI
+#define Ki 0.6
+#define Kp 0.5
 
-int etat;
-#define EtatAVANCE 0
-#define EtatRECULE 1
-#define EtatTOURNE 2
-#define EtatSTOP 3
+//Paramètre alimentation
+#define tensionBatterieMax 12
+#define pinTensionBatterie A1
+#define PDDBatterie 0.5
+double tensionBatterie = 12.0;
+
+//Parametres moteur lateraux
+#define sensMoteur1 5 //pin Speed moteur 1
+#define powerMoteur1 4 //pin sens moteur 1
+#define sensMoteur2 6 //pin Speed moteur 2   
+#define powerMoteur2 7 //pin sens moteur 2
+
+#define vitesseRotation 5 // deg/ms
+#define vitesseDeplacement 50 //entre 0 et 255
+int consigneVitesseDeplacement = 50;
+
+//Paramètres détection d'impacte
+#define pinImpactDroit 2 //Pin impacte droit
+#define pinImpactGauche 3 //Pin impacte gauche
+
+//Paramètres detection de périmetre
+#define pinDetectPerim 9 //Pin détection de périmètre
+
+int MODE = 0;
+//0 -> non renseigné
+//1 -> mode autonome
+//2 -> mode piloté
+
+bool autoIsStarted = false; //Le mode auto est démarré
+
+SoftwareSerial BLE(RxD,TxD); //Init bluetooth
 
 
-unsigned long time;
+//initialiser bluetooth shield par des commandes AT
+void setupBleConnection(){
 
-//initialiser bluetooth shield par 
-//des commandes AT
-void setupBleConnection()
-{
 	BLE.begin(9600); 
 	BLE.print("AT+CLEAR"); 
 	//définir le rôle du module bluetooth étant esclave
@@ -40,195 +60,199 @@ void setupBleConnection()
 	BLE.print("AT+SAVE1");
 	delay(1000);
 	//définir le nom du module
-	BLE.print("AT+NAMEtondeuse"); 
+	BLE.print("AT+NAMEtondeuseAuto"); 
 	delay(1000);
 	//définir le mot de passe du module bluetooth
-	BLE.print("AT+PIN1234"); 
+	BLE.print("AT+PIN0000"); 
 	delay(400);
 	Serial.println("OK conf BT");
 	BLE.println("BT OK");
 }
 
-void accelere(int sens, int v)
-{
-  	switch (sens)
-  	{
-		case DEVANT:
-			digitalWrite(M1,HIGH);       
-			digitalWrite(M2, HIGH);  
-		break;
-		case ARRIERE:
-			digitalWrite(M1,LOW);       
-			digitalWrite(M2, LOW);  
-		break;
-		case DROITE:
-			digitalWrite(M1,LOW);       
-			digitalWrite(M2, HIGH); 
-		break;
-		case GAUCHE:
-			digitalWrite(M1,HIGH);       
-			digitalWrite(M2, LOW);  
-		break;
+//stopper le robot
+void stop(){
+   	digitalWrite(sensMoteur1,LOW); //sens du moteur 1 en arriere
+    digitalWrite(sensMoteur2, LOW); //sens du moteur 2 en arriere
+    analogWrite(powerMoteur1, 0); //puissance du moteur 1 à 0
+    analogWrite(powerMoteur2, 0); //puissance du moteur 2 à 0
+}
+
+//pivoter à droite
+void right(int degre){
+
+	unsigned long start = millis(); // temps début
+	long dTime = degre/vitesseRotation; //en ms
+
+	while(millis() < start+dTime){
+		digitalWrite(sensMoteur1, LOW); //sens du moteur 1 en avant
+		digitalWrite(sensMoteur2, HIGH); //sens du moteur 2 en avant
+		analogWrite(powerMoteur1, 50); //puissance du moteur 1 à la consigne
+		analogWrite(powerMoteur2, 50); //puissance du moteur 2 à la consigne
 	}
-    
-	for (int i=50; i<=v; i++)
-	{
-		analogWrite(E1, i);   //PWM Speed Control   
-		analogWrite(E2, i);   //PWM Speed Control   
-		delay(10); 
+
+	stop(); //stopper la rotation
+}
+
+//pivoter à gauche
+void left(int degre){
+
+	unsigned long start = millis(); // temps début
+	long dTime = degre/vitesseRotation; //en ms
+
+	while(millis() < start+dTime){
+		digitalWrite(sensMoteur1, HIGH); //sens du moteur 1 en avant
+		digitalWrite(sensMoteur2, LOW); //sens du moteur 2 en avant
+		analogWrite(powerMoteur1, 50); //puissance du moteur 1 à la consigne
+		analogWrite(powerMoteur2, 50); //puissance du moteur 2 à la consigne
 	}
+
+	stop(); //stopper la rotation
 }
 
-void decelere(int v)
-{
-	for (int i=v; i>=0; i--)
-	{
-		analogWrite(E1, i);   //PWM Speed Control   
-		analogWrite(E2, i);   //PWM Speed Control   
-		delay(10); 
-	}
+//avancer
+void front(int vitesse){
+	digitalWrite(sensMoteur1, HIGH); //sens du moteur 1 en avant
+    digitalWrite(sensMoteur2, HIGH); //sens du moteur 2 en avant
+    analogWrite(powerMoteur1, vitesse); //puissance du moteur 1 à la consigne
+    analogWrite(powerMoteur2, vitesse); //puissance du moteur 2 à la consigne
 }
 
-
-void arret()
-{
-   	digitalWrite(M1,LOW);       
-    digitalWrite(M2, LOW);  
-    analogWrite(E1, 0);   //PWM Speed Control   
-    analogWrite(E2, 0);   //PWM Speed Control    
-}  
-
-int testImpact()
-{
-	int resu=0;
-	if (digitalRead(CGAUCHE)==0) resu=1;
-	if (digitalRead(CDROITE)==0) resu+=2;
-
-	if (resu==1) Serial.println("Impact GAUCHE");
-	if (resu==2) Serial.println("Impact DROIT");
-	if (resu==3) Serial.println("Impact DEVANT");
-	return resu;
+//reculer
+void back(int vitesse){
+	digitalWrite(sensMoteur1,LOW); //sens du moteur 1 en arriere
+    digitalWrite(sensMoteur2, LOW); //sens du moteur 2 en arriere
+    analogWrite(powerMoteur1, vitesse); //puissance du moteur 1 à la consigne
+    analogWrite(powerMoteur2, vitesse); //puissance du moteur 2 à la consigne
 }
-
-void resetChrono()
-{
-  	time=millis();
-}
-
-unsigned long chrono()
-{
-	unsigned long resu =millis()-time;
-	return resu;
-}
-
-
 
 void setup()  
 {      
-	pinMode(M1, OUTPUT);       
-	pinMode(M2, OUTPUT); 
-	pinMode(CGAUCHE, INPUT_PULLUP); 
-	pinMode(CDROITE, INPUT_PULLUP); 
-	Serial.begin(9600);
-	setupBleConnection();
-	
-	arret();
+	pinMode(pinTensionMoteur, INPUT); //Setup pin tension moteur de coupe
+	pinMode(pinMoteurCoupe, OUTPUT); //Setup pin commande moteur de coupe
 
-	etat=EtatSTOP;
-	//accelere(DEVANT,0);
-	resetChrono();
-} 
+	pinMode(pinTensionBatterie, INPUT); //Setup pin tension batterie
 
-int impa;
+	pinMode(sensMoteur1, OUTPUT); //Setup pin sens moteur 1
+	pinMode(sensMoteur2, OUTPUT); //Setup pin sens moteur 2
+	pinMode(powerMoteur1, OUTPUT); //Setup pin power moteur 1
+	pinMode(powerMoteur2, OUTPUT); //Setup pin power moteur 2
+
+	pinMode(pinImpactDroit, INPUT_PULLUP);
+	pinMode(pinImpactGauche, INPUT_PULLUP);
+
+	stop(); //Stopper le moteur à la mise sous tension
+	CommandeMoteurCoupe(0);
+
+	Serial.begin(9600); //Setup port serie
+	setupBleConnection(); //Setup bluetooth
+
+	//interruptions
+	MsTimer2::set(200, istrCommandeMoteurCoupe); //Interruption moteur coupe toutes les 200ms 
+	MsTimer2::start(); //Activer le timer 2
+}
 
 void loop() 
 {
-	int imp=testImpact();  
+	int impactDroit = digitalRead(pinImpactDroit);
+	int impactGauche = digitalRead(pinImpactGauche);
+	int detectPerim = digitalRead(pinDetectPerim);
+
+	if(impactGauche && impactDroit){
+		stop();
+		back(vitesseDeplacement);
+		delay(500);
+		right(90);
+		front(vitesseDeplacement);
+	}else if(impactGauche){
+		stop();
+		back(vitesseDeplacement);
+		delay(500);
+		right(30);
+		front(vitesseDeplacement);
+	}else if(impactDroit){
+		stop();
+		back(vitesseDeplacement);
+		delay(500);
+		left(30);
+		front(vitesseDeplacement);
+	}
+
+	if(detectPerim){
+		stop();
+		back(vitesseDeplacement);
+		delay(200);
+		left(90);
+		front(vitesseDeplacement);
+	}
+
 	if(BLE.available())
 	{
 		char c = BLE.read();
 		Serial.println(c);
-		if ((c>='0')&&(c<='9'))
-		{
-			vitesse=28*(c-'0');
-			etat=EtatAVANCE;
-			accelere(DEVANT,vitesse);
-			resetChrono();
-		}
-		
-		if (c=='a') 
-		{
-		etat=EtatAVANCE;
-		accelere(DEVANT,vitesse);
-		}
-		if (c=='s') 
-		{
-		etat=EtatSTOP; 
-		arret();
-		}
-		if (c=='g') 
-		{
-		etat=EtatSTOP; 
-		arret();  accelere(GAUCHE,vitesse);
-		}
-		if (c=='d') 
-		{
-		etat=EtatSTOP; 
-		arret();  accelere(DROITE,vitesse);
-		}
-		if (c=='r') 
-		{
-		etat=EtatRECULE;
-		accelere(ARRIERE,vitesse);
+		if ((c>='0')&&(c<='9')){
+			consigneVitesseDeplacement = map(c-'0', 0, 10, 0, 255);
+		}else if(c=='a'){
+			MODE = 1; //Mode automatique
+		}else if(c=='p'){
+			MODE = 2; //Mode piloté
+			autoIsStarted = false;
+		}else if(c=='d' && MODE==1 && !autoIsStarted){ //Démarrage en mode auto
+			front(vitesseDeplacement); //Démarrer le robot vers l'avant
+			autoIsStarted = true; //Le mode auto est démarré
+		}else if(c=='f' && MODE==2){
+			front(consigneVitesseDeplacement); //Déplacement vers l'avant
+		}else if(c=='b' && MODE==2){
+			back(consigneVitesseDeplacement); //Déplacement vers l'arrière
+		}else if(c=='r' && MODE==2){
+			right(30); //Pivot de 30 degrés à droite
+		}else if(c=='l' && MODE==2){
+			left(30); //Pivot de 30 degrés à gauche
+		}else if(c=='s'){
+			stop(); //Stopper le robot
 		}
 	}
+}
 
+//probleme timer et variables volatiles
+void istrCommandeMoteurCoupe(){
 
-    
-	switch (etat)
-	{
-		case EtatAVANCE:
-			if (chrono()>60000) 
-			{  
-				etat=EtatRECULE;
-				arret();
-				accelere(ARRIERE,vitesse);
-				resetChrono();
-			}
-			if (imp!=0) 
-			{  
-				impa=imp;
-				etat=EtatRECULE;
-				resetChrono();
-				arret();
-				accelere(ARRIERE,255);
-				
-			}
-			break;
-		
-		case EtatRECULE:
-			if (chrono()>2000) 
-			{ 
-				if (impa==GAUCHE )
-				{ etat=EtatTOURNE; arret();  accelere(DROITE,255); Serial.println(impa); }
-				else 
-				{ etat=EtatTOURNE; arret();  accelere(GAUCHE,255);Serial.println(impa); }
-				resetChrono();
-			}
-		break;
-		
-		case EtatTOURNE:
-			if (chrono()>200) 
-			{  
-				etat=EtatAVANCE;
-				arret();
-				accelere(DEVANT,vitesse);
-				resetChrono();
-			}
-		break;
-		
-		
-		break;
-		}
+	//Couper la tension aux bornes du moteur
+	CommandeMoteurCoupe(0);
+	
+	static unsigned long lastInterrup = 0.0; //sauvegarde temps derniere interruption
+	unsigned long now = millis(); //sauvegarder temps actuel
+	unsigned long dt = lastInterrup - now; //calcule dt entre 2 interruption
+	static double I_x = 0.0; //integrale
 
+	//Calcule de la tension du moteur
+  	double Vvitesse = (analogRead(pinTensionMoteur) * PDDMoteur)/Kmv;
+
+  	/******* Régulation PID ********/
+  	// Ecart entre la tension de la consigne et la mesure
+  	double ecart = Kmv * consigneVitesse - Vvitesse;
+ 
+  	// Calcul de la commande
+  	double commande = (Kp * ecart) + I_x;
+
+	//Terme intégral (sera utilisé lors du pas d'échantillonnage suivant)
+	I_x = I_x + Ki * dt * ecart;
+	/******* Fin régulation PID ********/
+
+	lastInterrup = now; //sauvegarde temps derniere interruption
+	CommandeMoteurCoupe(commande); //envoi de la commande au moteur
+
+	tensionBatterie = analogRead(pinTensionBatterie) * PDDBatterie; //acquisition de la tension de la batterie
+}
+
+void CommandeMoteurCoupe(double tensionMoy){
+
+	//eviter les saturations
+	if(tensionMoy<0){
+		tensionMoy=0;
+	}else if(tensionMoy>tensionBatterie){
+		tensionMoy = tensionBatterie;
+	}
+
+	//envoi de la commande au moteur en PWM
+	digitalWrite(pinMoteurCoupe, map(tensionMoy, 0, tensionBatterie, 0, 255));
 }
